c3c31cfead0413f6c85b0872460e258a61a5860b
Lukas <38840142+lukasstorck@users.noreply.github.com>
2026-03-09 23:33:14 +0100

    chore: bump version to 1.1

--- cpu_benchmark_score_display.user.js
+++ cpu_benchmark_score_display.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Geizhals CPU benchmark Scores
 // @namespace    geihhals_cpu_benchmark_scores
-// @version      1.0
+// @version      1.1
 // @description  Adds CPU benchmark scores from CPUBenchmark to Geizhals product listings
 // @match        https://geizhals.de/*
 // @source       https://github.com/lukasstorck/cpu-benchmark-score-display/raw/refs/heads/main/cpu_benchmark_score_display.user.js
